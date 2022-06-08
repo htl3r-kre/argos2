18b9ea40e4db206acca29a6c79c7b0c461ecbd4c
paulwaldecker <waldeckerpaul@gmail.com>
2022-06-08 22:37:50 +0200

    8.6.2022 22:37

--- src/main/java/com/pwm/argos/ArgosGUI.java
+++ src/main/java/com/pwm/argos/ArgosGUI.java
@@ -1,9 +1,43 @@
 package com.pwm.argos;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class ArgosGUI {
-    public static void main(String[] args) {
+import java.util.Objects;
+
+public class ArgosGUI extends Application {
+    double x, y = 0;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-view.fxml")));
+        Scene sc = new Scene(root);     //Scene
+        stage.setTitle("Argos");
+
+        //move around
+        root.setOnMousePressed(evt -> {
+            x = evt.getSceneX();
+            y = evt.getSceneY();
+        });
+        root.setOnMouseDragged(evt -> {
+            stage.setX(evt.getScreenX() - x);
+            stage.setY(evt.getScreenY() - y);
+        });
+
+        //Stage
+        stage.setScene(sc);
+        stage.show();
+
+
 
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
+
